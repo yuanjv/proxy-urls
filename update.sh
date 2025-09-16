a67cbfa4ef4b9b9dc5e9303f6e5abe094d4f058f
na <81983672+yuanjv@users.noreply.github.com>
2025-09-16 08:44:48 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,39 +4,40 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 rm proxy_urls.txt
 
-curl https://raw.githubusercontent.com/HakurouKen/free-node/main/public >> proxy_urls.txt
+curl https://raw.githubusercontent.com/HakurouKen/free-node/main/public | head -n 4096 >> proxy_urls.txt
 
-curl https://raw.githubusercontent.com/mfuu/v2ray/master/v2ray | base64 -d >> proxy_urls.txt
+curl https://raw.githubusercontent.com/mfuu/v2ray/master/v2ray | base64 -d | head -n 4096 >> proxy_urls.txt
 
 # REMOVED last update time: 2024-02-07 04:00
 # curl https://raw.githubusercontent.com/freefq/free/master/v2 | base64 -d >> proxy_urls.txt
 
 # NOT GREAT
-curl https://raw.githubusercontent.com/learnhard-cn/free_proxy_ss/main/free | base64 -d >> proxy_urls.txt
+curl https://raw.githubusercontent.com/learnhard-cn/free_proxy_ss/main/free | base64 -d | head -n 4096 >> proxy_urls.txt
 
-curl https://raw.githubusercontent.com/Pawdroid/Free-servers/main/sub | base64 -d >> proxy_urls.txt
+curl https://raw.githubusercontent.com/Pawdroid/Free-servers/main/sub | base64 -d | head -n 4096 >> proxy_urls.txt
 
 # REMOVED last update time: 2022-10-04 08:00
-#curl https://raw.githubusercontent.com/wrfree/free/main/v2 | base64 -d >> proxy_urls.txt
+#curl https://raw.githubusercontent.com/wrfree/free/main/v2 | base64 -d | head -n 4096 >> proxy_urls.txt
 
-curl https://raw.githubusercontent.com/aiboboxx/v2rayfree/main/v2 | base64 -d >> proxy_urls.txt
+curl https://raw.githubusercontent.com/aiboboxx/v2rayfree/main/v2 | base64 -d | head -n 4096 >> proxy_urls.txt
 # curl https://raw.githubusercontent.com/aiboboxx/v2rayfree/main/v1 >> proxy_urls.txt
 
-curl https://raw.githubusercontent.com/Epodonios/v2ray-configs/main/All_Configs_Sub.txt >> proxy_urls.txt
+curl https://raw.githubusercontent.com/Epodonios/v2ray-configs/main/All_Configs_Sub.txt | head -n 4096 >> proxy_urls.txt
 
-curl https://raw.githubusercontent.com/barry-far/V2ray-Config/refs/heads/main/All_Configs_Sub.txt >> proxy_urls.txt
+curl https://raw.githubusercontent.com/barry-far/V2ray-Config/refs/heads/main/All_Configs_Sub.txt | head -n 4096 >> proxy_urls.txt
 
-curl https://raw.githubusercontent.com/gfpcom/free-proxy-list/refs/heads/main/list/vless.txt >> proxy_urls.txt
-curl https://raw.githubusercontent.com/gfpcom/free-proxy-list/refs/heads/main/list/vmess.txt >> proxy_urls.txt
-curl https://raw.githubusercontent.com/gfpcom/free-proxy-list/refs/heads/main/list/trojan.txt >> proxy_urls.txt
-curl https://raw.githubusercontent.com/gfpcom/free-proxy-list/refs/heads/main/list/ss.txt >> proxy_urls.txt
+curl https://raw.githubusercontent.com/gfpcom/free-proxy-list/refs/heads/main/list/vless.txt | head -n 4096 >> proxy_urls.txt
+curl https://raw.githubusercontent.com/gfpcom/free-proxy-list/refs/heads/main/list/vmess.txt | head -n 4096 >> proxy_urls.txt
+curl https://raw.githubusercontent.com/gfpcom/free-proxy-list/refs/heads/main/list/trojan.txt | head -n 4096 >> proxy_urls.txt
+curl https://raw.githubusercontent.com/gfpcom/free-proxy-list/refs/heads/main/list/ss.txt | head -n 4096 >> proxy_urls.txt
 
-curl https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/refs/heads/main/all_extracted_configs.txt >> proxy_urls.txt
+curl https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/refs/heads/main/all_extracted_configs.txt | head -n 4096 >> proxy_urls.txt
 
 # format may not work
-curl https://raw.githubusercontent.com/roosterkid/openproxylist/refs/heads/main/V2RAY.txt >> proxy_urls.txt
+curl https://raw.githubusercontent.com/roosterkid/openproxylist/refs/heads/main/V2RAY.txt | head -n 4096 >> proxy_urls.txt
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+sort proxy_urls.txt | uniq > dupless_proxy_urls.txt
 
 rm -r XRAY_KNIFE
 mkdir XRAY_KNIFE
@@ -45,4 +46,4 @@ wget https://github.com/lilendian0x00/xray-knife/releases/download/v7.1.3/Xray-k
 unzip Xray-knife-linux-64.zip
 cd ..
 ln -s XRAY_KNIFE/xray-knife xray-knife
-./xray-knife http -f proxy_urls.txt -s -p -o alive_proxy_urls.txt
+./xray-knife http -f dupless_proxy_urls.txt -s -p -o alive_proxy_urls.txt
